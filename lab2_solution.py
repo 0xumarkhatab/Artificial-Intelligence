93bff1781cf2074467e205cd804cb73ce1bf0e09
Umar Khatab <umarkhatab467@gmail.com>
2022-03-05 10:17:16 +0500

    Added Task 2

--- lab2_solution.py
+++ lab2_solution.py
@@ -5,3 +5,19 @@ def removeValue(sampleList, val):
   pass
 print(removeValue([1,2,3,2,23,4,1,2],2))
 # LAB (end solution)
+
+# Task 2
+def findAndReplaceValue(sampleList, val):
+  
+  length = len(sampleList);
+  counter=0;
+
+
+  while counter<length:
+    if(sampleList[counter]==val):
+      sampleList[counter]=200
+    counter=counter+1;
+  pass
+  return sampleList;  
+  # LAB (end solution)
+print(findAndReplaceValue([1,2,3,2,23,4,1,2],2 ))
