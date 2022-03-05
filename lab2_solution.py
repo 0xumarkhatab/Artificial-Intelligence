6b5f7688936da71e1359dd97be515e84c00a75b1
Umar Khatab <umarkhatab467@gmail.com>
2022-03-05 10:16:41 +0500

    Added Task 1

---
+++ lab2_solution.py
@@ -0,0 +1,7 @@
+# Task 1
+def removeValue(sampleList, val):
+  retValue =  [ num for num in sampleList if num!=val]
+  return retValue
+  pass
+print(removeValue([1,2,3,2,23,4,1,2],2))
+# LAB (end solution)
