88e7c644cdc5dbbf59de6d58be66ec6084c2689c
Umar Khatab <umarkhatab467@gmail.com>
2022-03-12 02:55:16 +0000

    Added Question 2 in the lab3 solution

--- Lab3/solution.py
+++ Lab3/solution.py
@@ -2,7 +2,16 @@
 # 19L-2765
 # Section B
 # Lab3 
-#Question 1
+# Question 1
 def symmetric_difference(a,b):
   return a.symmetric_difference(b)
 symmetric_difference({1,2,3},{3,4,5})
+
+# Questoin 2
+def divide(p,q):
+  try:
+    return p/q
+  except ZeroDivisionError:
+    print(f"Division by Zero Error")
+divide(1,0)
+divide(1,2)
