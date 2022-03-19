dc1e7b4c61bde4c5a4de00b7c66f249ce3b176a4
Umar Khatab <umarkhatab467@gmail.com>
2022-03-19 09:25:43 +0500

    Filter for Invalid Words Applied in Question1

--- Lab4/solution.py
+++ Lab4/solution.py
@@ -1,4 +1,20 @@
+# Question 1
+import re
 
+def isAccepted(string):
+ 
+    # Make own character set and pass
+    # this as argument in compile method
+    regex = re.compile('[0123456789@_!#$%^&*()<>?/\|}{~:]')
+     
+    # Pass the string in search
+    # method of regex object.   
+    if(regex.search(string) == None):
+        return True
+         
+    else:
+        return False
+   
 def getWords(filename):
   fname="/content/"+filename;
   file=open(fname,"r")
