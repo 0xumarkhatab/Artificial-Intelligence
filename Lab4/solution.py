e3db29d8b14c352d74424b2f449900f7cc2851b8
Umar Khatab <umarkhatab467@gmail.com>
2022-03-19 09:24:30 +0500

    Words read in lowercase Question1

--- Lab4/solution.py
+++ Lab4/solution.py
@@ -1,1 +1,14 @@
 
+def getWords(filename):
+  fname="/content/"+filename;
+  file=open(fname,"r")
+  content=file.read()
+  content=content.lower()
+  return content
+
+
+
+print(getWords("file1.txt"))
+print(getWords("file2.txt"))
+print(getWords("file3.txt"))
+print(getWords("file4.txt"))
