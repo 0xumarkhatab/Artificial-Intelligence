e863f16b767883c4589ca79f02e81cc20b5cabc3
Umar Khatab <umarkhatab467@gmail.com>
2022-03-19 10:10:32 +0500

    Probability calculation Done in Q1

--- Lab4/solution.py
+++ Lab4/solution.py
@@ -1,6 +1,7 @@
 # Question 1
 import re
 from collections import deque
+
 def removeNumeralWords(string):
  
     # Make own character set and pass
@@ -30,7 +31,7 @@ def getWords(filename):
   content=file.read()
   content=content.lower()
   content= content.split(sep=' ') # separating words
-  words=[]
+  words=deque()
   for x in content:
     if (removeNumeralWords(x)==True):
       purified = givePureEnglish(x)
@@ -40,8 +41,42 @@ def getWords(filename):
 
 
 
-content1 = getWords("file1.txt")
+class wordProbability:
+  "This Class Stores The Word , Count and It's Probabilty "
+  word=None
+  count=-1
+  probability=-1
+  def __init__(self, w, c,p):
+    self.word=w
+    self.count=c
+    self.probability=p
+  def print(self):
+    print('',self.word,' ',self.count,' ',self.probability)
 
+
+
+
+def countProbability(words):
+  nonduplicated=set(content1) # removing duplicates
+  length=len(words)
+  probabilities=deque()
+  for x in nonduplicated:
+    wordCount=words.count(x)  
+    eleProb=" "+str(wordCount)+"/"+str(length)
+    element=wordProbability(x,wordCount,length)
+    probabilities.append(element)
+
+  return probabilities
+
+content1 = getWords("file1.txt")
 content2 = getWords("file2.txt")
 content3 = getWords("file3.txt")
 content4 = getWords("file4.txt")
+
+for x in countProbability(content1):
+  x.print()
+
+
+
+
+
